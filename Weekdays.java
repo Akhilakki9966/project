4433266486bc879a6ffe3e1e9e8e45f2f51bdb43
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-05-22 13:11:27 +0530

    Create Weekdays.java

---
+++ Weekdays.java
@@ -0,0 +1,10 @@
+public class Weekdays {
+    public static void main(String[] args) {
+        String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        System.out.println("The weekdays are:");
+        for (String day : weekdays) {
+            System.out.println(day);
+        }
+    }
+}
