6a514336ab10c90a80410622c165cff0a91cfba2
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-05-25 02:09:25 +0530

    Create Weekdays.java

---
+++ Weekdays.java
@@ -0,0 +1,14 @@
+// Java program to display weekday names
+public class Weekdays {
+    public static void main(String[] args) {
+        // Array of weekday names
+        String[] weekdays = {
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+        };
+
+        // Loop to print each weekday
+        for (String day : weekdays) {
+            System.out.println(day);
+        }
+    }
+}
