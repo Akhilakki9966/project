8166cde6095b048cfc5f587511231e34ab2fb074
Akhilakki9966 <Akhilakki9966@gmail.com>
2025-05-24 15:13:15 +0530

    Create FreestyleJava.java

---
+++ FreestyleJava.java
@@ -0,0 +1,6 @@
+public class FreestyleJava {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Freestyle Java Coding!");
+        System.out.println("Let's make coding fun and creative!");
+    }
+}
